aeaa19650f62b5b4b80aa34f176d1f1df323d3ba
Timothy Francis Rogers <me@timothyfrancisrogers.me>
2019-07-22 10:41:01 -0400

    working set of stuff

--- scripts/download-firestorm.py
+++ scripts/download-firestorm.py
@@ -1,16 +1,14 @@
 #!/usr/bin/env python3
 
 import os.path
-from urllib.request import urlretrieve
 import subprocess
 import os
 import re
 
 def main():
-    urlretrieve("http://downloads.firestormviewer.org/linux/Phoenix_Firestorm-releasex64_x86_64_6.2.4.57588.tar.xz", "Phoenix_Firestorm-releasex64_x86_64_6.2.4.57588.tar.xz")
+    subprocess.call(['wget','https://downloads.firestormviewer.org/linux/Phoenix_Firestorm-releasex64_x86_64_6.2.4.57588.tar.xz'])
 
     subprocess.call(['tar','-xf','Phoenix_Firestorm-releasex64_x86_64_6.2.4.57588.tar.xz'])
 
     subprocess.call(['rm','-rf','Phoenix_Firestorm-releasex64_x86_64_6.2.4.57588.tar.xz'])
-
 main()
